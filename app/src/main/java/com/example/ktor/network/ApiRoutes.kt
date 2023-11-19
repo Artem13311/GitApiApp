ca86f0f75aceb7dd56b94e8bf63b80503c61a721
Artem13311 <103060499+Artem13311@users.noreply.github.com>
2023-11-19 16:19:49 +0300

    code

--- app/src/main/java/com/example/ktor/network/ApiRoutes.kt
+++ app/src/main/java/com/example/ktor/network/ApiRoutes.kt
@@ -3,5 +3,5 @@ package com.example.ktor.network
 
 object ApiRoutes {
     private const val BASE_URL = "https://api.github.com"
-    const val REPOSITORY = "$BASE_URL/repos/Artem13311/handh"
+    const val REPOSITORY = "$BASE_URL/repos/Artem13311/GitApiApp"
 }
